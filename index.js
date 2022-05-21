ed38777d7261e930a7e9135d729ac710df337b81
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-05-21 14:21:40 +0600

    Limit one booking per user per treatment per day

--- index.js
+++ index.js
@@ -8,6 +8,56 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.53zjc.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const servicesCollection = client
+      .db("doctors_portal")
+      .collection("services");
+    const bookingCollection = client
+      .db("doctors_portal")
+      .collection("bookings");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    /**
+     * Api naming convention
+     * app.get('/booking') -- get all the booking on this collection or by filter / query get more than one
+     * app.get('/booking/:id') -- get specific booking
+     * app.post('/booking') -- add a new booking
+     * app.patch('/booking/:id')
+     * app.delete('/booking/:id')
+     */
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patient: booking.patient,
+      };
+      const exist = await bookingCollection.findOne(query);
+      if (exist) {
+        return res.send({ success: false, booking: exist });
+      }
+      const result = await bookingCollection.insertOne(booking);
+      res.send({ success: true, booking: result });
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
