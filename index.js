96e0c0c03cf125d89fd8d42f929a753683a05f3e
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-05-23 17:54:29 +0600

    delete api created

--- index.js
+++ index.js
@@ -158,9 +158,21 @@ async function run() {
       res.send({ success: true, booking: result });
     });
 
+    app.get("/doctor", verifyJWT, verifyAdmin, async (req, res) => {
+      const doctors = await doctorCollection.find().toArray();
+      res.send(doctors);
+    });
+
     app.post("/doctor", verifyJWT, verifyAdmin, async (req, res) => {
       const doctor = req.body;
       const result = await doctorCollection.insertOne(doctor);
+      res.send(result);
+    });
+
+    app.delete("/doctor/:email", verifyJWT, verifyAdmin, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const result = await doctorCollection.deleteOne(query);
       res.send(result);
     });
   } finally {
