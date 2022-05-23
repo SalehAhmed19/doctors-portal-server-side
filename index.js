7334ed191a5abe778eb659588949dc39a1b211df
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-05-23 06:00:11 +0600

    ensure the security of all user route

--- index.js
+++ index.js
@@ -85,6 +85,13 @@ async function run() {
       res.send({ result: result, accessToken: token });
     });
 
+    app.get("/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user.role === "admin";
+      res.send({ admin: isAdmin });
+    });
+
     app.get("/user", verifyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
